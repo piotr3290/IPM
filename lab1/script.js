a03efff53c29b609e01388c74fd149ff491df3ca
piotr3290 <piotr3290@wp.pl>
2022-10-08 21:01:02 +0200

    directory input

--- lab1/script.js
+++ lab1/script.js
@@ -6,6 +6,7 @@ const data = [
     ['125.98.0.63', '255.8.60.77'],
     ['https://www.google.com/', 'https://www.microsoft.com/pl-pl'],
     ['C:\\windows\\temp', 'd:\\msdos'],
+    ['C:\\WinDows\\temp', 'd:\\msdOs'],
     ['/etc/passwd', '/etc/hosts'],
     ['4344:1bba:bfb9:5d94:cbdd:80bb:b523:e823', '89e1:53a6:7a2a:e1d0:05f3:dc78:4fb9:22de'],
     ['997', '+48 555 666 777'],
@@ -13,12 +14,21 @@ const data = [
 
 
 function fillData() {
-    let a = document.getElementsByTagName('input');
-    for (let i = 0; i < a.length; i++) {
-        a[i].value = data[i][randomInt()];
+    let inputs = document.getElementsByTagName('input');
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = data[i][randomInt()];
     }
 }
 
 function randomInt() {
     return Math.floor(Math.random() * (2));
+}
+
+function check() {
+    let input = document.getElementById('directory-path2-input');
+    if (input.value.match(/[A-Za-z]:(\\(windows|winnt|win|dos|msdos))(\\(\w)+)*/i)) {
+        input.setCustomValidity('');
+    } else {
+        input.setCustomValidity('invalid');
+    }
 }
