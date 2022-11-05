4ee1f57715e085cf1bad487922449352948cf2b9
piotr3290 <piotr3290@wp.pl>
2022-11-05 18:15:36 +0100

    dragging functions

--- lab3/script.js
+++ lab3/script.js
@@ -14,22 +14,32 @@ function initPage() {
 }
 
 function handleStartDrag(event) {
+    event.target.classList.add('dragging');
+    draggingElement = this;
 }
 
 function handleEndDrag(event) {
+    event.target.classList.remove('dragging');
 }
 
 function handleEnterDrag(event) {
+    event.target.classList.add('dragging-over');
 }
 
 function handleLeaveDrag(event) {
+    event.target.classList.remove('dragging-over');
 }
 
 function handleDragOver(event) {
-
+    event.preventDefault();
+    return false;
 }
 
 function handleDrop(event) {
+    event.stopPropagation();
 
+    event.target.classList.remove('dragging-over');
+
+    draggingElement = null;
 }
 
