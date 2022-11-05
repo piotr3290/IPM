8d13cc579be238b02113b5b48b376b27389b0cbe
piotr3290 <piotr3290@wp.pl>
2022-11-05 18:37:47 +0100

    blocks generate

--- lab3/script.js
+++ lab3/script.js
@@ -47,5 +47,18 @@ function handleDrop(event) {
 }
 
 function generateBlock() {
+    let resp = document.getElementById('resp');
+    let newBlock = document.createElement('div');
+    newBlock.style.backgroundColor = randomColor();
+    newBlock.classList.add('block');
 
+    resp.appendChild(newBlock);
+}
+
+function randomColor() {
+    return `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+}
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
